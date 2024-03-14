912e4abf4f7fe76ae476fca020691f24fe112818
Atul Koirala <62100593+Atulkoirala@users.noreply.github.com>
2024-03-14 13:04:02 +0545

    Update settings.py

--- sechaWear/settings.py
+++ sechaWear/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
